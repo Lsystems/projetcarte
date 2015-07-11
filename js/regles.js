8a05b89e6de1230ade63dea92b641fbaf8ddad28
Lukia <lukiathetrue@yahoo.fr>
2015-07-11 23:00:18 +0200

    sécurisation du tour avant résolution

--- js/regles.js
+++ js/regles.js
@@ -5,7 +5,9 @@ function reglesDeDepart(){
 	this.nbCarteJoueur=7,
 	this.distributeur=0, // joueur distributeur en début de partie
 	this.joueur=1, // joueur au tour
-	this.endTour=0; // nombre de jeu avant la fin du tour
+	this.endTour=0, // nombre de jeu avant la fin du tour
+	this.enseigneDemande, // l'enseigne demandée lors du tour
+	this.cartejouee=[] // le tableau des cartes qui seront jouée prendant le tour
 }
 
 // après la distribution et à chaque fin de tour
@@ -23,16 +25,40 @@ function tirage(sabot){
 }
 
 function tour(regles,clickedCard){
-		// si la carte vient de la main du joueur en cours
-		if(parseInt($(clickedCard).parents(".joueur").data("joueur"))==regles.joueur){
-			// on récupère l'objet de la carte chez le joueur concerné
-			var carteIndex=parseInt($(clickedCard).data("index"));
-			var carte=joueurArray[regles.joueur].main[carteIndex];
-			console.log(carte,joueurArray[regles.joueur].main[carteIndex]);
-			
-			carte=new tradCarte(carte);
+		
+	// si la carte vient de la main du joueur en cours
+	if(parseInt($(clickedCard).parents(".joueur").data("joueur"))==regles.joueur){
+		
+		// on récupère l'objet de la carte chez le joueur concerné
+		var carteIndex=parseInt($(clickedCard).data("index"));
+		var main=joueurArray[regles.joueur].main;
+		var carte=main[carteIndex];
+		// on assigne la couleur pour le tour pour la premiere carte jouée du tour
+		if(regles.endTour==0){
+			regles.enseigneDemande=carte.enseigne;
+		}
+		
+		//vérification du réglementaire de la carte
+		var legal=true;
+		if(carte.enseigne!=regles.enseigneDemande){
+			// on check la main du joueur
+			for(var i=0; i<main.length; i++){
+				// s'il a la couleur demandée
+				if(main[i].enseigne==regles.enseigneDemande){
+					alert("Vous devez jouer la couleur demandée !");
+					legal=false;
+					break;
+				}
+			}
+		}
+		// si la carte est réglementaire
+		if(legal==true){
+			// on rentre la carte dans le tableau des cartes jouées pour ce tour
+			regles.cartejouee.push(carte);
+			// on traduit la carte pour l'affichage
+			displayCard=new tradCarte(carte);
 			// on l'ajoute au tapis
-			$("#tapis .joueur[data-joueur="+regles.joueur+"]").append('<div>'+carte.tradCardFigure+' '+carte.tradCardEnseigne+'</div>');
+			$("#tapis .joueur[data-joueur="+regles.joueur+"]").append('<div>'+displayCard.tradCardFigure+' '+displayCard.tradCardEnseigne+'</div>');
 			
 			// on retire la carte de la main du joueur
 			joueurArray[regles.joueur].main.splice(carteIndex,1);
@@ -57,8 +83,12 @@ function tour(regles,clickedCard){
 			}else {
 				//si le tour est terminé
 				
-				//résolution();
+				resolution();
 			}
 		}
-	
+	}
+} // end tour
+
+function resolution(){
+
 }
